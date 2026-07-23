f9da4cf2779124ef41f78709b860de36d789eb68
张雄 <zhangxiong5@asiainfo.com>
2026-07-23 10:34:00 +0800

    feat: 原子操作

---
+++ GO_BASIC/concurrence/atomic.go
@@ -0,0 +1,46 @@
+package concurrence
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+var n int32 = 0
+
+func inc1() {
+	n++
+}
+
+func inc2() {
+	atomic.AddInt32(&n, 1)
+}
+
+func Atomic() {
+	const P = 1000 //开大量协程才能把脏写问题测出来
+	wg := sync.WaitGroup{}
+	wg.Add(P)
+	for range P {
+		go func() {
+			defer wg.Done()
+			inc1()
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("finally n=%d\n", n) //多运行几次，n经常不等于1000
+	fmt.Println("==========================")
+	n = 0 //重置n
+
+	wg = sync.WaitGroup{}
+	wg.Add(P)
+	for range P {
+		go func() {
+			defer wg.Done()
+			inc2()
+			// \_ = n + 1
+			// \_ = atomic.LoadInt32(&n) //即使在高并发环境下，读一个变量也没必要用atomic.Load
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("finally n=%d\n", n)
+}
---
+++ GO_BASIC/concurrence/main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"go_basic/concurrence"
+)
+
+func main() {
+	concurrence.Atomic()
+}
